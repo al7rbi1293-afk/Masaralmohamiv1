3040d14d73be71863d3b19333bcd7e9bdddc20c5
al7rbi1293-afk <al7rbi1293@gmail.com>
2026-03-31 17:42:42 +0300

    feat: add TikTok pixel to web root layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { IBM_Plex_Sans_Arabic, Inter } from 'next/font/google';
+import Script from 'next/script';
 import { Footer } from '@/components/layout/footer';
 import { Navbar } from '@/components/layout/navbar';
 import { ThemeProvider } from '@/components/layout/theme-provider';
@@ -25,6 +26,7 @@ const inter = Inter({
 const siteUrl = getPublicSiteUrl();
 const isVercelDeployment = Boolean(process.env.VERCEL);
 const defaultOgImage = '/masar-logo.png';
+const tiktokPixelId = 'D75TMPBC77UBIUFTUUVG';
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
@@ -97,6 +99,46 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body className={`${ibmPlexArabic.variable} ${inter.variable} min-h-screen antialiased`}>
+        <Script id="tiktok-pixel-init" strategy="beforeInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject = t;
+              var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "holdConsent", "revokeConsent", "grantConsent"];
+              ttq.setAndDefer = function (target, method) {
+                target[method] = function () {
+                  target.push([method].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+              };
+              for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+              ttq.instance = function (id) {
+                var instance = ttq._i[id] || [];
+                for (var n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(instance, ttq.methods[n]);
+                return instance;
+              };
+              ttq.load = function (id, options) {
+                var url = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                var partner = options && options.partner;
+                ttq._i = ttq._i || {};
+                ttq._i[id] = [];
+                ttq._i[id]._u = url;
+                ttq._t = ttq._t || {};
+                ttq._t[id] = +new Date();
+                ttq._o = ttq._o || {};
+                ttq._o[id] = options || {};
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.async = true;
+                script.src = url + "?sdkid=" + id + "&lib=" + t;
+                var firstScript = document.getElementsByTagName("script")[0];
+                firstScript.parentNode.insertBefore(script, firstScript);
+              };
+
+              ttq.load('${tiktokPixelId}');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
         <ThemeProvider>
           <a href="#main-content" className="skip-link">
             تجاوز إلى المحتوى
